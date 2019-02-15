9f7a8146dd83dd495b66944ffc7789f657c7580f
Sonia Mianji <soniamianji1@gmail.com>
2019-02-15 20:00:16 +0100

    styles added

--- src/MessageList.js
+++ src/MessageList.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 class MessageList extends React.Component {
   render() {
     return (
-      <ul className="message-list list-group d-flex justify-content-center mx-auto text-secondary">
+      <ul className="listParent message-list list-group d-flex justify-content-center mx-auto text-secondary scrollspy-example mt-3">
         {this.props.messages.map((message, index) => {
           return (
             <li className="list-group-item" key={message.id}>
-              <div>{message.senderId}</div>
+              <div className="badge badge-warning mb-2">{message.senderId}</div>
               <div>{message.text}</div>
             </li>
           );
--- src/SendMessageForm.js
+++ src/SendMessageForm.js
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 class SendMessageForm extends React.Component {
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit} className="send-message-form ">
+      <form
+        onSubmit={this.props.handleSubmit}
+        className="mx-auto d-flex justify-content-center"
+      >
         <input
-          className="w-100 "
+          className="form-control w-75"
           onChange={this.props.handleChange}
           value={this.props.message}
           placeholder="Type your message and hit ENTER"
